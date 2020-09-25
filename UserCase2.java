61f8a8f7ba7f5286061d762602aedb66f9ddd2b9
vahbib <singhbibhav084@gmail.com>
2020-09-25 21:58:47 +0530

    Added UserCase2

---
+++ UserCase2.java
@@ -0,0 +1,11 @@
+package SnakeLadder;
+
+public class UserCase2 {
+		
+		public static void main(String[] args) {
+
+				int die = (int) (Math.random() * 6) + 1;
+				System.out.println("The Dice Number is "+die+".");
+			
+		}
+}
